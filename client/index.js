67043942b3ef5b65fdb5a008d9b852b4a128077e
Balodya <magicpilya@yandex.ru>
2024-01-25 03:26:22 +0300

    Довели до ума :D

--- client/index.js
+++ client/index.js
@@ -1,82 +1,115 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  const socket = io("localhost:3001");
+  const outputElement = document.getElementById("output");
+  const playState = document.getElementById("state");
+  const turnElement = document.getElementById("turn");
 
-    const socket = io('localhost:3001');
-    const outputElement = document.getElementById('output');
-    const playState = document.getElementById("state");
-    const turnElement = document.getElementById("turn");
+  const modal = document.querySelector(".modal");
+  const overlay = document.querySelector(".overlay");
+  const closeModalBtn = document.querySelector(".btn-close");
 
-    writeToOutput('Подключение стабильно', socket);
-    writeState('Вы играете за O');
-    turnElement.innerHTML = 'Сейчас ходит X'
+  writeToOutput("Подключение стабильно", socket);
+  writeState("Вы играете за O");
+  turnElement.innerHTML = "Сейчас ходит X";
 
+  let players = [];
+  let currentPlayer = "";
+  let myTurn = false;
 
-    let players = [];
-    let currentPlayer = '';
-    let myTurn = false;
+  const boardElement = document.getElementById("board");
 
-    const boardElement = document.getElementById('board');
+  function writeToOutput(message) {
+    outputElement.innerHTML += `<p>${message}</p>`;
+  }
 
-    function writeToOutput(message) {
-        outputElement.innerHTML += `<p>${message}</p>`;
+  function writeState(message) {
+    playState.innerHTML = message;
+  }
+
+  function createBoard() {
+    for (let i = 0; i < 9; i++) {
+      const cell = document.createElement("div");
+      cell.className = "cell";
+      cell.dataset.index = i;
+      cell.addEventListener("click", () => handleCellClick(i));
+      boardElement.appendChild(cell);
     }
+  }
 
-    function writeState(message) {
-        playState.innerHTML = message;
-    }
-
-
-    function createBoard() {
-        for (let i = 0; i < 9; i++) {
-            const cell = document.createElement('div');
-            cell.className = 'cell';
-            cell.dataset.index = i;
-            cell.addEventListener('click', () => handleCellClick(i));
-            boardElement.appendChild(cell);
-        }
-    }
-
-    function updateBoard(updatedBoard) {
-        const cells = document.querySelectorAll('.cell');
-        cells.forEach((cell, index) => {
-            cell.textContent = updatedBoard[index];
-        });
-    }
-
-    function handleCellClick(index) {
-        if (myTurn) {
-            socket.emit('makeMove', index);
-        } else {
-            alert("Не ваш ход!"+ currentPlayer);
-        }
-    }
-
-    socket.on('updatePlayers', (updatedPlayers) => {
-        players = updatedPlayers;
+  function updateBoard(updatedBoard) {
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach((cell, index) => {
+      cell.textContent = updatedBoard[index];
     });
+  }
 
-    socket.on('turnH', (turnH) =>{
-        turnElement.innerHTML = `<p>Сейчас ходит ${turnH}</p>`
-    })
+  function handleCellClick(index) {
+    if (myTurn) {
+      socket.emit("makeMove", index);
+    } else {
+      modal.innerHTML = "";
+      modal.innerHTML += `Не ваш ход!`;
+    }
+  }
 
-    socket.on('yourTurn', (turn) => {
-        console.log('Текущий ход: ', turn);
-        writeState('Вы играете за ' + turn);
-        currentPlayer = turn;
-        myTurn = (socket.id === players[0] && currentPlayer === 'X') || (socket.id === players[1] && currentPlayer === 'O');
-    });
+  socket.on("updatePlayers", (updatedPlayers) => {
+    players = updatedPlayers;
+  });
 
-    socket.on('updateBoard', (updatedBoard) => {
-        updateBoard(updatedBoard);
-    });
+  socket.on("turnH", (turnH) => {
+    turnElement.innerHTML = `<p>Сейчас ходит ${turnH}</p>`;
+  });
 
-    socket.on('gameOver', ({ winner, board }) => {
-        if (winner === 'draw') {
-            alert('Ничья!');
-        } else {
-            alert(`Игрок ${winner} победил!`);
-        }
-        updateBoard(board);
-    });
+  socket.on("yourTurn", (turn) => {
+    console.log("Текущий ход: ", turn);
+    writeState("Вы играете за " + turn);
+    currentPlayer = turn;
+    myTurn =
+      (socket.id === players[0] && currentPlayer === "X") ||
+      (socket.id === players[1] && currentPlayer === "O");
+  });
 
-    createBoard();
+  socket.on("updateBoard", (updatedBoard) => {
+    updateBoard(updatedBoard);
+  });
+
+  socket.on("gameOver", ({ winner, board }) => {
+    if (winner === "draw") {
+      modal.innerHTML = "";
+      modal.innerHTML += `НИЧЬЯ`;
+      // alert('Ничья!');
+      openModal();
+    } else {
+      // alert(`Игрок ${winner} победил!`);
+      modal.innerHTML = "";
+      modal.innerHTML += `Игрок ${winner} победил!`;
+      openModal();
+    }
+    updateBoard(board);
+  });
+
+  createBoard();
+
+  // close modal function
+  const closeModal = function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+  };
+
+  // close the modal when the close button and overlay is clicked
+  closeModalBtn.addEventListener("click", closeModal);
+  overlay.addEventListener("click", closeModal);
+
+  // close modal when the Esc key is pressed
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+      closeModal();
+    }
+  });
+
+  // open modal function
+  const openModal = function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  };
 });
--- server.js
+++ server.js
@@ -1,94 +1,104 @@
-const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io');
+const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-app.use(express.static('client'));
+app.use(express.static("client"));
 
 let players = [];
 let board = Array(9).fill(null);
-let currentPlayer = 'X';
+let currentPlayer = "X";
 let nameUser = [];
 
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
+  nameUser.push(socket.id);
+  console.log("Список пользователей в сети:", nameUser[0], nameUser[1]);
 
-    nameUser.push(socket.id)
-    console.log('Список пользователей в сети:', nameUser[0], nameUser[1])
+  if (players.length < 2) {
+    resetGame();
+  }
 
-    if (players.length < 2 ) {
-        resetGame()
+  players.push(socket.id);
+  io.emit("updatePlayers", players);
+
+  io.to(players[0]).emit("yourTurn", currentPlayer);
+
+  let turnH = " ";
+
+  socket.on("makeMove", (index) => {
+    if (
+      socket.id === players[0] &&
+      currentPlayer === "X" &&
+      board[index] === null
+    ) {
+      turnH = "O";
+      board[index] = "X";
+      io.emit("updateBoard", board);
+      checkWinner();
+      currentPlayer = "O";
+      io.to(players[1]).emit("yourTurn", currentPlayer);
+    } else if (
+      socket.id === players[1] &&
+      currentPlayer === "O" &&
+      board[index] === null
+    ) {
+      turnH = "X";
+      board[index] = "O";
+      io.emit("updateBoard", board);
+      checkWinner();
+      currentPlayer = "X";
+      io.to(players[0]).emit("yourTurn", currentPlayer);
     }
+    io.emit("turnH", turnH);
+  });
 
-    players.push(socket.id);
-    io.emit('updatePlayers', players);
-
-    io.to(players[0]).emit('yourTurn', currentPlayer);
-
-    let turnH = ' ';
-
-    socket.on('makeMove', (index) => {
-        if (socket.id === players[0] && currentPlayer === 'X' && board[index] === null) {
-            turnH = "O"
-            board[index] = 'X';
-            io.emit('updateBoard', board);
-            checkWinner();
-            currentPlayer = 'O';
-            io.to(players[1]).emit('yourTurn', currentPlayer);
-        } else if (socket.id === players[1] && currentPlayer === 'O' && board[index] === null) {
-            turnH = "X"
-            board[index] = 'O';
-            io.emit('updateBoard', board);
-            checkWinner();
-            currentPlayer = 'X';
-            io.to(players[0]).emit('yourTurn', currentPlayer);
-        }
-        io.emit('turnH', turnH);
-    });
-
-
-
-    socket.on('disconnect', () => {
-        players = players.filter(player => player !== socket.id);
-        io.emit('updatePlayers', players);
-        console.log('Отключился пользователь ID:', socket.id);
-    });
+  socket.on("disconnect", () => {
+    players = players.filter((player) => player !== socket.id);
+    io.emit("updatePlayers", players);
+    console.log("Отключился пользователь ID:", socket.id);
+  });
 });
 
 function checkWinner() {
-    const winningCombinations = [
-        [0, 1, 2], [3, 4, 5], [6, 7, 8],
-        [0, 3, 6], [1, 4, 7], [2, 5, 8],
-        [0, 4, 8], [2, 4, 6]
-    ];
+  const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
-    for (const combination of winningCombinations) {
-        const [a, b, c] = combination;
+  for (const combination of winningCombinations) {
+    const [a, b, c] = combination;
 
-        if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-            const winner = board[a];
-            io.emit('gameOver', { winner, board });
-            resetGame();
-            return;
-        }
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      const winner = board[a];
+      io.emit("gameOver", { winner, board });
+      resetGame();
+      return;
     }
+  }
 
-    if (board.every(cell => cell !== null)) {
-        io.emit('gameOver', { winner: 'draw', board });
-        resetGame();
-    }
+  if (board.every((cell) => cell !== null)) {
+    io.emit("gameOver", { winner: "draw", board });
+    resetGame();
+  }
 }
 
 function resetGame() {
-    board = Array(9).fill(null);
-    currentPlayer = 'X';
-    io.emit('updateBoard', board);
-    io.to(players[0]).emit('yourTurn', currentPlayer);
+  board = Array(9).fill(null);
+  currentPlayer = "X";
+  io.emit("updateBoard", board);
+  io.to(players[0]).emit("yourTurn", currentPlayer);
 }
 
 const port = process.env.PORT || 3001;
 server.listen(port, () => {
-    console.log(`Сервер начал работу на PORT: ${port}`);
+  console.log(`Сервер начал работу на PORT: ${port}`);
 });
